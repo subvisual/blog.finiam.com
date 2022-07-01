38c6dcbf93a47948ab1d2e2bd3e79cd18e4efb2d
Paula Carneiro <paulacarneiro215@gmail.com>
2022-07-01 13:30:25 +0100

    Replace ternary operator with conditional statement

--- components/featured-posts/featured-posts.tsx
+++ components/featured-posts/featured-posts.tsx
@@ -4,11 +4,15 @@ import PostPreview from '../post-preview/post-preview';
 
 import styles from './featured-posts.module.scss';
 
+type FeaturedPostsProps = {
+  posts: PostPreview[];
+};
+
 export default function FeaturedPosts({ posts }: FeaturedPostsProps) {
   return (
     <div className={styles['featured-posts']}>
       {posts.map((post, idx) => (
-        <PostPreview key={post.slug.current} post={post} isFirst={idx === 0 ? true : false} />
+        <PostPreview key={post.slug.current} post={post} isFirst={idx === 0} />
       ))}
     </div>
   );
