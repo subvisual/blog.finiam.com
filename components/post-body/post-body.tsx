536d56e1e2bd7e2a58ab639962e9cb8cd1b96b66
Paula Carneiro <paulacarneiro215@gmail.com>
2022-07-01 19:40:01 +0100

    Style post body

--- components/post-body/post-body.tsx
+++ components/post-body/post-body.tsx
@@ -1,4 +1,8 @@
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import rehypeRaw from 'rehype-raw';
+
+import styles from './post-body.module.scss';
 
 type PostBodyProps = {
   data: {
@@ -8,8 +12,32 @@ type PostBodyProps = {
 
 export default function PostBody({ data: { body } }: PostBodyProps) {
   return (
-    <article className='post-body'>
-      <ReactMarkdown>{body}</ReactMarkdown>
+    <article className={styles['post-body']}>
+      <ReactMarkdown
+        rehypePlugins={[rehypeRaw]}
+        className={styles.reactMarkdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || 'latex');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                customStyle={{
+                  backgroundColor: 'white',
+                }}
+                language={match[1]}
+                PreTag='div'
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {body}
+      </ReactMarkdown>
     </article>
   );
 }
